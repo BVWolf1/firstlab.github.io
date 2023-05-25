cfe3c5e5d90507629281dab1cbd9baa2b39c9690
BVWolf1 <73719677+BVWolf1@users.noreply.github.com>
2023-05-25 16:47:53 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+/// Code Challenge 1- Alert Output
+alert("Hello World");
+
+alert("JavaScript is awesome!")
+
+alert(42 + 42);
+
+/// Code Challenge 2 - myName variable + output to console
+let myName = "Brandy Veal-Wolfe";
+alert(myName);
+console.log(myName);
+
+// Code Challenge 3 - create a calculate Area function that takes length * width
+
+function calculateArea(length, width) {
+  area = length * width
+  document.write("<p>The area of " + length + " length and " + width + " width is " + area + ".</p>");
+  
+}
+calculateArea(100, 15);
+calculateArea(25, 15);
+
+alert(area)
+
+// Code Challenge 4 - getElementById
+
+let el = document.getElementById("practice");
+el.textContent = "My favorite TV series is Star Trek Next Generation."
+
+
+
+
